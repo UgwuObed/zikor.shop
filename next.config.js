8a5f4ce94e7023a8bc3bddbd6890bbabaecde791
UgwuObed <vjazzyvictor@gmail.com>
2025-05-07 12:08:27 +0100

    refactor: Simplify slug handling in API by removing subdomain detection and unused rewrites

--- next.config.js
+++ next.config.js
@@ -1,18 +1,4 @@
-export default {
-    async rewrites() {
-      return [
-        {
-          source: '/:path*',
-          has: [
-            {
-              type: 'host',
-              value: '(?<slug>.+)\\.zikor\\.shop',
-            },
-          ],
-          destination: '/store/:slug/:path*',
-        },
-      ];
-    },
+module.exports = {
     images: {
       domains: ['res.cloudinary.com'],
     },
