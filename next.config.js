a31ee0f8ebee51708c60dd63f70014840d319fde
UgwuObed <vjazzyvictor@gmail.com>
2025-05-07 10:55:10 +0100

    feat: Update next.config.js to use ES module syntax and enhance URL rewrites for store routing

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
-module.exports = {
+export default {
+    async rewrites() {
+      return [
+        {
+          source: '/:path*',
+          has: [
+            {
+              type: 'host',
+              value: '(?<slug>.+)\\.zikor\\.shop',
+            },
+          ],
+          destination: '/store/:slug/:path*',
+        },
+      ];
+    },
     images: {
       domains: ['res.cloudinary.com'],
     },
