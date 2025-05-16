7140c358e30109b6f066352882da4f51ccf15683
UgwuObed <vjazzyvictor@gmail.com>
2025-05-16 20:10:13 +0100

    fix(product): change Content-Type header to application/json in EditProductForm submission

--- src/components/Store/[slug].tsx
+++ src/components/Store/[slug].tsx
@@ -242,7 +242,6 @@ const StorefrontPage = () => {
             className="w-16 h-16 border-4 border-dashed rounded-full animate-spin mx-auto mb-4"
             style={{ borderTopColor: themeColor }}
           ></div>
-          <p className="text-gray-500">Loading storefront...</p>
         </div>
       </div>
     )
