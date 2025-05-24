8a170bc73b4f85518d31ef4af9b3f436fefbca90
UgwuObed <vjazzyvictor@gmail.com>
2025-05-24 18:28:11 +0100

    refactor: update StorefrontPage to handle slug as an array for improved searchParams processing

--- src/app/storefront/page.tsx
+++ src/app/storefront/page.tsx
@@ -2,10 +2,11 @@
 import Store from "../../components/Store/[slug]";
 import "../../app/globals.css";
 
-export default function StorefrontPage({ searchParams }: { searchParams: URLSearchParams }) {
-  const slug = searchParams.get("slug");
+export default function StorefrontPage({ searchParams }: { searchParams: { [key: string]: string | string[] | undefined } }) {
+  const slug = Array.isArray(searchParams.slug) ? searchParams.slug[0] : searchParams.slug;
 
   if (!slug) return <div>No storefront slug provided</div>;
 
   return <Store slug={slug} />;
 }
+
