03eca38f6cfa496c8def049d6a4968ca116fffe0
UgwuObed <vjazzyvictor@gmail.com>
2025-05-24 18:25:26 +0100

    refactor: simplify StorefrontPage component by removing unused code and props

--- src/app/storefront/page.tsx
+++ src/app/storefront/page.tsx
@@ -1,19 +1,11 @@
 // app/storefront/page.tsx
-"use client"
-import { useSearchParams } from "next/navigation";
 import Store from "../../components/Store/[slug]";
 import "../../app/globals.css";
 
-export interface StoreProps {
-  slug: string;
-}
-
-export default function StorefrontPage() {
-  const searchParams = useSearchParams();
-  const slug = searchParams?.get("slug");
+export default function StorefrontPage({ searchParams }: { searchParams: URLSearchParams }) {
+  const slug = searchParams.get("slug");
 
   if (!slug) return <div>No storefront slug provided</div>;
 
-  // Pass the slug to the Store component
   return <Store slug={slug} />;
 }
